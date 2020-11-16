3b052e6203bfc6dfd4178e3fac011e04e11d6532
Daria Papunina <popunina@mail.ru>
2020-11-16 21:15:46 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 10000;
+        int fix = 20;
+        int mille = cost / fix;
+        System.out.println(mille);
+
+    }
+}
+
